cb6a5bfac847fd4310cd220437b8d1d9ea5647b6
NataliaColome <nmcolome@gmail.com>
2020-03-08 21:56:56 -0600

    Add styling for NewsArticle

--- src/components/NewsArticle/NewsArticle.js
+++ src/components/NewsArticle/NewsArticle.js
@@ -7,9 +7,11 @@ function NewsArticle({id, headline, img, description, url}) {
       <div>
         <img className="article-img" src={img} alt="image related to the new"/>
       </div>
-      <h2>{headline}</h2>
-      <p>{description}</p>
-      <a href={url}>Link to article</a>
+      <h2 className="article-headline">{headline}</h2>
+      <p className="article-description">{description}</p>
+      <div className="article-link">
+        <a href={url}>Link to article</a>
+      </div>
     </div>
   )
 }
