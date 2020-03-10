f6d77f216badbfe3ca2293540a76df42f8463ba7
NataliaColome <nmcolome@gmail.com>
2020-03-09 20:04:00 -0600

    Add styling to NewsArticles

--- src/components/NewsArticle/NewsArticle.js
+++ src/components/NewsArticle/NewsArticle.js
@@ -2,13 +2,14 @@ import React from 'react';
 import './NewsArticle.css';
 
 function NewsArticle({id, headline, img, description, url}) {
+  const css = {backgroundImage: `url(${img})`}
   return (
     <div className={`article-card article-${id}`}>
-      <div>
-        <img className="article-img" src={img} alt="image related to the new"/>
+      <div className="img-container" style={css}></div>
+      <div className="content">
+        <h2 className="article-headline">{headline}</h2>
+        <p className="article-description">{description}</p>
       </div>
-      <h2 className="article-headline">{headline}</h2>
-      <p className="article-description">{description}</p>
       <div className="article-link">
         <a href={url}>Link to article</a>
       </div>
